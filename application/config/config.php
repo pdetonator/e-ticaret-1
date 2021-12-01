94c709c08fe1d4ba4b074a9c83cbc3b48b8d01d7
unlcemre <unlcemre0@gmail.com>
2021-12-01 20:32:13 +0300

    Türkçe Dil Paketi Eklendi.

--- application/config/config.php
+++ application/config/config.php
@@ -76,7 +76,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+$config['language']	= 'turkish';
 
 /*
 |--------------------------------------------------------------------------
@@ -448,12 +448,15 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = FALSE;
-$config['csrf_token_name'] = 'csrf_test_name';
+$config['csrf_protection'] = TRUE;
+$config['csrf_token_name'] = 'csrf__token';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
-$config['csrf_exclude_uris'] = array();
+$config['csrf_exclude_uris'] = array(
+    'user/login',
+    'user/register'
+);
 
 /*
 |--------------------------------------------------------------------------
